0fdcc8cc8e4fbf6c229745ca68252b41b63d3ef8
asagi <shinigami.asagi@gmail.com>
2016-06-11 10:10:20 +0200

    actual floortile

---
+++ HGE Project/FloorTile.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "FloorTile.h"
+
+
+FloorTile::FloorTile()
+{
+}
+
+
+FloorTile::~FloorTile()
+{
+}
---
+++ HGE Project/FloorTile.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "CommonUtilities\Vector2.h"
+class FloorTile
+{
+public:
+	FloorTile();
+	~FloorTile();
+	CU::Vector2i myPosition;
+};
+
