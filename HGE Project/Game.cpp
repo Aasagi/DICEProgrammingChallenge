c4f2f342d627b14d5c78f273753d285e9b9b5dce
asagi <shinigami.asagi@gmail.com>
2016-06-11 11:19:42 +0200

    Random floor generation

--- HGE Project/Game.cpp
+++ HGE Project/Game.cpp
@@ -4,15 +4,18 @@
 #include "SpriteRenderCommand.h"
 #include "Megaton.h"
 #include "Button.h"
+#include "time.h"
 #include "MainMenu.h"
 #include "Renderer\FontRenderCommand.h"
 
 #define WINDOW_WIDTH 800.0f
 #define WINDOW_HEIGHT 600.0f
 #define TILE_SIZE 20.0f
+#define START_AREA_TILE_NUMBER 5
 
 Game::Game(void)
 {
+	
 }
 
 Game::~Game(void)
@@ -22,7 +25,7 @@ Game::~Game(void)
 
 void Game::Init()
 {
-
+	srand(time(NULL));
 	GenerateRandomFloor();
 }
 
@@ -64,13 +67,57 @@ void Game::Render()
 
 void Game::GenerateRandomFloor()
 {
+	bool recentlyMadeHole = false;
+	int lastTileHeight = 1;
 
 	int numTiles = WINDOW_WIDTH / TILE_SIZE;
 	myFloorTiles.Init(numTiles);
 	for (int floorIndex = 0; floorIndex < numTiles; floorIndex++)
 	{
-		myFloorTiles.Add(FloorTile());
-		myFloorTiles[floorIndex].myPosition = CU::Vector2f(floorIndex*TILE_SIZE, WINDOW_HEIGHT - TILE_SIZE);
+		bool makeFloor = false;
+
+		if (floorIndex < START_AREA_TILE_NUMBER)
+		{
+			makeFloor = true;
+			lastTileHeight = 1;
+		}
+		else if (recentlyMadeHole)
+		{
+			makeFloor = true;
+		}
+
+		if (makeFloor == false)
+		{
+			int randomTile = rand();
+			if (randomTile % 2 == 0)
+			{
+				makeFloor = true;
+
+				if (lastTileHeight < 5 && randomTile % 3 == 0 )
+				{
+					lastTileHeight++;
+				}
+				else if (lastTileHeight > 1 && randomTile % 5 == 0)
+				{
+					lastTileHeight--;
+				}
+			}
+			else
+			{
+				recentlyMadeHole = true;
+			}
+		}
+
+		if (makeFloor)
+		{
+			recentlyMadeHole = false;
+			for (int tileHeightIndex = 1; tileHeightIndex <= lastTileHeight; tileHeightIndex++)
+			{
+				myFloorTiles.Add(FloorTile());
+				myFloorTiles[myFloorTiles.Count() - 1].myPosition = CU::Vector2f(floorIndex*TILE_SIZE, WINDOW_HEIGHT - TILE_SIZE * tileHeightIndex);
+			}
+		}
+
 	}
 
 	
